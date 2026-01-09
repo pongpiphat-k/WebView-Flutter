02651f8be42cf62bbcea04ce85b70e5b02ac1b5e
pongpipha-k <pongpiphat_k@cmu.ac.th>
2026-01-09 23:36:21 +0700

    WebView load content and navigation

--- lib/main.dart
+++ lib/main.dart
@@ -34,14 +34,62 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   late WebViewController _controller1;
   late WebViewController _controller2;
+  late WebViewController _controller3;
 
-// create 2 controllers for webview
+  bool _isLoading = false;
+
+// create 3 controllers for webview
   @override
   void initState() {
     super.initState();
-    _controller = WebViewController();
-    _controller.loadRequest(Uri.parse('https://flutter.dev/'));
-      
+    
+
+    _controller1 = WebViewController()
+    ..setJavaScriptMode(JavaScriptMode.unrestricted)
+    ..setNavigationDelegate(
+      NavigationDelegate(
+        onPageStarted: (url) {
+          setState(() {
+            _isLoading = true;
+          });
+          print("Page started loading: $url");
+        },
+        onPageFinished: (url) {
+          setState(() {
+            _isLoading = false;
+          });
+          print("Page finished loading: $url");
+        },
+        onNavigationRequest: (request) {
+          if (request.url.startsWith("https://flutter.dev") || 
+              request.url.startsWith("https://docs.flutter.dev")) {
+            return NavigationDecision.navigate;
+          }
+          print("Blocking navigation to ${request.url}");
+          return NavigationDecision.prevent;
+        },
+
+      ),
+    )
+    ..loadRequest(Uri.parse("https://flutter.dev"));
+
+    _controller2 = WebViewController()
+    ..setJavaScriptMode(JavaScriptMode.unrestricted)
+    ..loadFlutterAsset('assets/index.html');
+
+    _controller3 = WebViewController()
+    ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..loadHtmlString('''
+        <html>
+        <head>
+          <title>HTML String</title>
+        </head>
+        <body>
+          <h1>This is loaded from a HTML string!</h1>
+          <p>Hello, WebView Kong</p>
+        </body>
+        </html>
+      ''');
   }
 
   @override
@@ -49,9 +97,41 @@ class _MyHomePageState extends State<MyHomePage> {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
+        title: Text("WebView Navigation & Events"),
+        actions: [
+        IconButton(
+          icon: Icon(Icons.arrow_back),
+          onPressed: () async {
+            if (await _controller1.canGoBack()) {
+              _controller1.goBack();
+            }
+          }, ),
+        IconButton(
+          icon: Icon(Icons.arrow_forward),
+          onPressed: () async {
+            if (await _controller1.canGoForward()) {
+              _controller1.goForward();
+            }
+          }, ),
+        IconButton(
+          icon: Icon(Icons.refresh),
+          onPressed: () => _controller1.reload(), ),
+        ],
       ),
-      body: WebViewWidget(controller: _controller),
-    );
+      body: Stack(
+        children: [
+            Column(
+        children: [
+          Expanded(
+            child: WebViewWidget(controller: _controller1),
+          ),
+        ],
+      ),
+      if (_isLoading)
+        const LinearProgressIndicator(),
+    ],
+  ),
+    
+  );
   }
 }
